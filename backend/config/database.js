ec0a5b45bca01cc8f6ac7cd5741d353ee6fe5301
RyanSamman <RyanSamman@outlook.com>
2020-08-18 17:13:59 +0300

    Added w=true to mongoose connection by default
    This allows you to leave it out when setting the heroku envs

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(`${process.env.DATABASE_URI}&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 const db = mongoose.connection;
 db.on('connected', () => {
     console.log(`mongoose connected to database at ${db.host}:${db.port}`);
