6744b0cdd93ac67413c14c5d84dfa7bcf94fb1f5
Anton Yershov <anthony.yershov@gmail.com>
2020-08-18 03:41:24 -0700

    added build back in and removed it from .gitignore

---
+++ backend/build/precache-manifest.b558d2c9ebba0426921716e305c087d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cf60353b9939093bf7d4daa0cc6187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7556136c14c7618da03",
+    "url": "/static/js/2.b3e85a5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3e85a5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764feeed67f9c8142040",
+    "url": "/static/js/main.6bf88a1e.chunk.js"
+  },
+  {
+    "revision": "2194c2353035891e7bca",
+    "url": "/static/js/runtime-main.c705307b.js"
+  }
+]);
