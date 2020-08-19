e05293bc3445e1c9fd1f8db1e0af6d4c6db765e4
RyanSamman <RyanSamman@outlook.com>
2020-08-19 07:06:37 +0300

    Error Handling Middleware

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const config = require('./config');
 const logger = require('./util/logger');
 const express = require('express');
-const { requestLogger } = require('./util/middleware')
 const app = express();
+require('express-async-errors');
+const { requestLogger, errorHandler } = require('./util/middleware');
+
 const cors = require('cors');
 
 require('./config/database');
@@ -21,6 +23,10 @@ app.use(express.static('build'));
 
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
+
+// Add 404 route here
+
+app.use(errorHandler);
 
 app.listen(config.PORT, () => {
 	logger.info(`Server started on http://localhost:${config.PORT}`);
--- backend/util/middleware.js
+++ backend/util/middleware.js
@@ -1,10 +1,23 @@
+const config = require('./../config');
 const logger = require('./logger');
+const jwt = require('jsonwebtoken');
 
 const requestLogger = (req, res, next) => {
 	logger.silly(`${req.method} ${req.path} ${JSON.stringify(req.body)}`);
 	next();
 };
 
+
+const errorHandler = (err, req, res) => {
+	if (err.name === 'JsonWebTokenError') return res.status(401).json({ error: 'Invalid JWT'});
+	
+	// Add errors here while we find them
+	logger.error(`${req.method} ${req.path} - ${JSON.stringify(req.body)}`, err);
+	res.status(500).json({ error: err.message, errorname: err.name });
+};
+
 module.exports = {
 	requestLogger,
+	checkAuthToken,
+	errorHandler,
 };
