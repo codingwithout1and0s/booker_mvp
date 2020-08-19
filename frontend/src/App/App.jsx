8a08e2b08df4d7e0a0779028311cc04a4ffb814e
Anton Yershov <anthony.yershov@gmail.com>
2020-08-18 23:13:36 -0700

    added authenticated routes

---
+++ frontend/src/App/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import authService from '../services/authService';
+
+const PrivateRoute = ({ cb, ...rest }) => (
+  <Route {...rest}
+    render={props => {
+      let component = cb(props);
+      if(!!component.props.user) return component;
+      else return <Redirect to="/signup" />;
+    }}
+  />
+);
+
+function App() {
+  const [user, setUser] = useState(authService.getUser() || undefined);
+
+  const handleLogin = () => {
+    let user = authService.getUser() || undefined;
+    this.setState({ user });
+  };
+
+  const handleLogout = () => {
+    authService.logout()
+    this.setState({ user: undefined });
+  };
+
+  return (
+    <div className="App">
+      
+      <Router>
+        <Switch>
+          <Route exact path="/" render={props => <div>Landing Page</div>} />
+          <Route path="/login" render={props => <div>Login Page</div>} />
+          <Route path="/signup" render={props => <div>Signup Page</div>} />
+          <Route path="/onboarding" render={props => <div>Onboarding Page</div>} />
+          <PrivateRoute
+            exact
+            path="/swipe"
+            user={user}
+            cb={props => <div>Swipe Page</div>}
+          />
+          <PrivateRoute
+            path="/profile/:username"
+            user={user}
+            render={props => <div>Profile Page: {props.match.params.username}</div>}
+          />
+          <PrivateRoute
+            path="/matches"
+            user={user}
+            render={props => <div>Matches Page</div>}
+          />
+          <PrivateRoute
+            path="/group/:groupName"
+            user={user}
+            render={props => <div>Matches Page: {props.match.params.groupName}</div>}
+          />
+        </Switch>
+      </Router>
+
+    </div>
+  );
+}
+
+export default App;
